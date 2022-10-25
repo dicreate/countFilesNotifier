f3aaa1a0e6dc09fe3453f458e985fcb9984b347d
DmitriyKovchugo <dkovchugo@mail.ru>
2022-10-25 15:45:43 +0300

    feat: add dictionary

--- script.py
+++ script.py
@@ -2,12 +2,21 @@ import os, os.path
 import time
 
 dir_path='E:/python/test'
+currentDir = ''
+dictFiles = {}
+currentValue = 0
 
 def countFiles():
    for element in os.listdir(dir_path):
       element_path = os.path.join(dir_path, element)
       if os.path.isdir(element_path):
-         print(element_path, sum(1 for element in os.scandir(element_path) if element.is_file))
+         currentDir = os.path.split(element_path)[-1]
+         if (currentDir in dictFiles.keys()):
+            currentValue = dictFiles[currentDir]
+         dictFiles[currentDir] = sum(1 for files in os.scandir(element_path) if files.is_file)
+         if(currentValue == dictFiles[currentDir]):
+            print('значение не изменилось')
+         print(dictFiles)   
    time.sleep(5)
 
 while True: 
