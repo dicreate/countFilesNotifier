c3c54cf92e4694b607822e5cb28dbc5dd9fcd400
DmitriyKovchugo <dkovchugo@mail.ru>
2022-11-22 23:15:30 +0300

    feat: add messageBox

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import os, os.path
 import time
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton
 import sys
 
 dir_path='E:/python/test'
@@ -13,21 +13,35 @@ class Window(QMainWindow):
    def __init__(self):
       super(Window,self).__init__()
 
-      self.setWindowTitle("Простая программа")
+      self.setWindowTitle("notifier")
       self.setGeometry(300, 250, 350, 200)
 
       self.main_text = QtWidgets.QLabel(self)
-      self.main_text.setText("Это базовая надпись")
+      self.main_text.setText("Скрипт работает")
       self.main_text.move(100,100)
       self.main_text.adjustSize()
 
+      self.btn = QtWidgets.QPushButton(self)
+      self.btn.move(70, 150)
+      self.btn.setText('Нажми на меня')
+      self.btn.setFixedWidth(200)
+      self.btn.clicked.connect(self.showMessage)   
+
+   def add_label(self):
+      print('add')
+
+   def showMessage(self):
+      self.message = QMessageBox(self)
+      self.message.setIcon(QMessageBox.Critical)
+      self.message.setText("Information ")
+      self.message.setWindowTitle("Information MessageBox")
+      self.message.show()
 
 def application():
    app = QApplication(sys.argv)
    window = Window()
    window.show()
    sys.exit(app.exec_())
-
 
 def countFiles():
    for element in os.listdir(dir_path):
@@ -45,5 +59,4 @@ def countFiles():
 
 #while True: 
 #   countFiles()
-
 application()
