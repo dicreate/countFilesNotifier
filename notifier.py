fdff9071f35ebf9f61db30c236f15677a3e4704d
DmitriyKovchugo <dkovchugo@mail.ru>
2022-12-02 17:35:59 +0300

    feat: add logs && console fixes

--- notifier.py
+++ notifier.py
@@ -1,6 +1,7 @@
 import os, os.path, sys, time, datetime
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from threading import Thread
 
 defaultfont = QtGui.QFont('Times New Roman', 30)
 
@@ -8,7 +9,8 @@ class Window(QMainWindow): # класс Window на основе класса QM
    def __init__(self): # конструктор
       super(Window, self).__init__() # Метод super для наследования базовых классов 
       self.dir_path = 'D:/VIDEO/TRASSA/EXPORT' # Папка со всеми объектами 
-      self.interval = 1080
+      self.interval = 10 # Интервал работы скрипты
+      self.clearConsoleInterval = 30 # Интервал очистки консоли
 
       self.currentDir = '' # Папка, с которой работает скрипт в настоящий момент времени
       self.dictFiles = {} # Объект, в котором хранятся все директории и количество файлов в них
@@ -40,64 +42,94 @@ class Window(QMainWindow): # класс Window на основе класса QM
       self.btnClearAll.setFixedWidth(230) # Ширина
       self.btnClearAll.setFixedHeight(50) # Высота
       self.btnClearAll.clicked.connect(self.disable_current) # Вызов функции по клику "clear_all"
-      print(f"[{datetime.datetime.now()}]: Скрипт запущен \n Путь к директорию c объектами: {self.dir_path} \n Интервал работы: {self.interval} секунд (18 минут)")
 
-      while True: 
-         self.showMessage() # Запуск функции на бесконечное выполнение
+      print(f"[{datetime.datetime.now()}]: Скрипт запущен \n Путь к директорию c объектами: {self.dir_path} \n Интервал работы: {self.interval} секунд ({self.interval / 60} минут) \n Очищение консоли через: {self.clearConsoleInterval} секунд ({self.clearConsoleInterval / 60} минут) ")
+
+      self.writeToFile(f"[{datetime.datetime.now()}]: Скрипт запущен \n Путь к директорию c объектами: {self.dir_path} \n Интервал работы: {self.interval} секунд ({self.interval / 60} минут) \n")
+      
+      self.clear_console = Thread(target=self.clear_console_func).start()
+      self.showMessage()
+   
+   def __del__(self): # Деструктор
+      self.file.close()
+   
+
+   def writeToFile(self, text): # Функция записи в файл
+      try:
+         self.file = open(f'{self.dir_path}/notifierlog.txt', 'a', encoding='utf-8')
+         self.file.write(text)
+         self.file.close()
+      except Exception as e:
+         print(f"[{datetime.datetime.now()}]: {e}")
 
    def clear_all(self): # Очистка объекта notifications
       self.notifications = {}
       print(f'[{datetime.datetime.now()}]: Уведомления по всем объектам включены')
+      self.writeToFile(f'[{datetime.datetime.now()}]: Уведомления по всем объектам включены \n')
 
    def clear_current(self): # Очистка значения объекта notifications
       self.notifications[self.currentDir] = 0
       print(f"[{datetime.datetime.now()}]: Уведомления по объекту {self.currentDir} включены")
+      self.writeToFile(f"[{datetime.datetime.now()}]: Уведомления по объекту {self.currentDir} включены \n")
 
    def disable_current(self): # Отключение уведомлений по объекту
       self.notifications[self.currentDir] = 100
       print(f"[{datetime.datetime.now()}]: Уведомления по объекту {self.currentDir} выключены")
+      self.writeToFile(f"[{datetime.datetime.now()}]: Уведомления по объекту {self.currentDir} выключены \n")
+
+   def clear_console_func(self): # функция очистки консоли
+      time.sleep(self.clearConsoleInterval)
+      os.system('cls||clear')
+      print(f"[{datetime.datetime.now()}]: Консоль была очищена")    
 
    def showMessage(self): # Объявление функции
-      try: 
-         for element in os.listdir(self.dir_path): # Цикл, считывающий все элементы, находящиеся в папке
-            self.element_path = os.path.join(self.dir_path, element) # Получаем ссылку на элемент
-            
-            if os.path.isdir(self.element_path): # Проверка является ли элемент директорием 
-               self.lastValue = 0 # Прошлое количество файлов
-               self.lastNotifications = 0 # Прошлое количество уведомлений
-               self.currentDir = os.path.split(self.element_path)[-1] # Имя текущего директория
-
-               if (self.currentDir in self.dictFiles.keys()): # Проверка есть ли информация по данному директорию в объекте
-                  self.lastValue = self.dictFiles[self.currentDir] # Записываем прошлое значение в переменную
+      while True:
+         try: 
+            for element in os.listdir(self.dir_path): # Цикл, считывающий все элементы, находящиеся в папке
+               self.element_path = os.path.join(self.dir_path, element) # Получаем ссылку на элемент
                
-               if (self.currentDir in self.notifications.keys()): # Проверка были ли уже уведомления по объекту
-                  self.lastNotifications = self.notifications[self.currentDir] # Записываем прошлое значение в переменную
+               if os.path.isdir(self.element_path): # Проверка является ли элемент директорием 
+                  self.lastValue = 0 # Прошлое количество файлов
+                  self.lastNotifications = 0 # Прошлое количество уведомлений
+                  self.currentDir = os.path.split(self.element_path)[-1] # Имя текущего директория
 
-               self.dictFiles[self.currentDir] = sum(1 for files in os.scandir(self.element_path) if files.is_file) # Получаем текущее количество файлов
-               print(f"[{datetime.datetime.now()}]: Объект: {self.currentDir}, Кол-во файлов: {self.dictFiles[self.currentDir]}")
-
-               if (self.lastValue == self.dictFiles[self.currentDir]): # Сравниваем прошлое и текущее значение
-                  self.notifications[self.currentDir] = self.lastNotifications + 1 # Увеличение количества уведомлений на единицу
-
-                  if (self.notifications[self.currentDir] < 3) : # Сообщение об уведомлениях
-                     self.notificationMessage =  f"<span style='color: green;'>Серия уведомлений: {self.notifications[self.currentDir]}</span></p>"
-                  else:
-                     self.notificationMessage =  f"<span style='color: red;'>Серия уведомлений: {self.notifications[self.currentDir]}</span></p><p style='color: red; font-size: 18px'>Последнее уведомление. Необходимо возобновить уведомления, если хотите продолжить их получать</p>"
+                  if (self.currentDir in self.dictFiles.keys()): # Проверка есть ли информация по данному директорию в объекте
+                     self.lastValue = self.dictFiles[self.currentDir] # Записываем прошлое значение в переменную
                   
-                  if (self.notifications[self.currentDir]) < 4:
-                     self.message.setText(f"<p style='color: red; font-size: 100px;'>ВНИМАНИЕ !!!</p><p style='margin-top: 80px'> Кол-во файлов в папке не изменилось</p><p><span style='color: red; font-size: 80px;'>{self.currentDir}</span></p>\n \
-                     <p style='margin-top: 60px';>{self.notificationMessage}") # Текст сообщения 
-                     self.message.setWindowTitle(f"Объект: {self.currentDir}    Дата и время: {str(datetime.datetime.now())}") # Заголовок сообщения
-                     self.message.show() # Вызов сообщения
-                     self.retval = self.message.exec_() # Корректное завершение
-                     print(f"[{datetime.datetime.now()}]: Количество файлов в папке {self.currentDir} не изменилось") 
-                  else: 
-                     print(f"[{datetime.datetime.now()}]: Количество файлов в папке {self.currentDir} не изменилось. Уведомление отключено")
-               else:
-                  self.notifications[self.currentDir] = 0 # Обнуляем количество уведомлений
-      except Exception as e:
-         print(f"[{datetime.datetime.now()}]: {e}")
-      time.sleep(self.interval) # Интервал, через который работает скрипт
+                  if (self.currentDir in self.notifications.keys()): # Проверка были ли уже уведомления по объекту
+                     self.lastNotifications = self.notifications[self.currentDir] # Записываем прошлое значение в переменную
+
+                  self.dictFiles[self.currentDir] = sum(1 for files in os.scandir(self.element_path) if files.is_file) # Получаем текущее количество файлов
+
+                  if (self.lastValue == self.dictFiles[self.currentDir]): # Сравниваем прошлое и текущее значение
+                     self.notifications[self.currentDir] = self.lastNotifications + 1 # Увеличение количества уведомлений на единицу
+
+                     if (self.notifications[self.currentDir] < 3) : # Сообщение об уведомлениях
+                        self.notificationMessage =  f"<span style='color: green;'>Серия уведомлений: {self.notifications[self.currentDir]}</span></p>"
+                     else:
+                        self.notificationMessage =  f"<span style='color: red;'>Серия уведомлений: {self.notifications[self.currentDir]}</span></p><p style='color: red; font-size: 18px'>Последнее уведомление. Необходимо возобновить уведомления, если хотите продолжить их получать</p>"
+                     
+                     if (self.notifications[self.currentDir]) < 4:
+                        print(f"[{datetime.datetime.now()}]: Количество файлов в папке {self.currentDir} не изменилось \n {datetime.datetime.now()}]: Ожидание ответа ...") 
+                        self.writeToFile(f"[{datetime.datetime.now()}]: Количество файлов в папке {self.currentDir} не изменилось \n {datetime.datetime.now()}]: Ожидание ответа ... \n") 
+                        self.message.setText(f"<p style='color: red; font-size: 100px;'>ВНИМАНИЕ !!!</p><p style='margin-top: 80px'> Кол-во файлов в папке не изменилось</p><p><span style='color: red; font-size: 80px;'>{self.currentDir}</span></p>\n \
+                        <p style='margin-top: 60px';>{self.notificationMessage}") # Текст сообщения 
+                        self.message.setWindowTitle(f"Объект: {self.currentDir}    Дата и время: {str(datetime.datetime.now())}") # Заголовок сообщения
+                        self.message.show() # Вызов сообщения
+                        self.retval = self.message.exec_() # Корректное завершение
+                        print(f"[{datetime.datetime.now()}]: Ответ обработан")
+                        self.writeToFile(f"[{datetime.datetime.now()}]: Ответ обработан")
+                     else: 
+                        print(f"[{datetime.datetime.now()}]: Количество файлов в папке {self.currentDir} не изменилось. Уведомление отключено")
+                        self.writeToFile(f"[{datetime.datetime.now()}]: Количество файлов в папке {self.currentDir} не изменилось. Уведомление отключено \n")
+                  else:
+                     print(f"[{datetime.datetime.now()}]: Объект: {self.currentDir}, Кол-во файлов: {self.dictFiles[self.currentDir]}")
+                     self.writeToFile(f"[{datetime.datetime.now()}]: Объект: {self.currentDir}, Кол-во файлов: {self.dictFiles[self.currentDir]} \n")
+                     self.notifications[self.currentDir] = 0 # Обнуляем количество уведомлений
+
+         except Exception as e:
+            print(f"[{datetime.datetime.now()}]: {e}")
+         time.sleep(self.interval) # Интервал, через который работает скрипт
 
 def application(): # Приложение
    app = QApplication(sys.argv) # Инициализация приложения
